166ef2583c1db9c282913bfda04d366e085c171a
Vladislav <VladislavPozhar@yandex.ru>
2022-07-02 15:19:02 +0400

    Initial commit

---
+++ src/test/java/com/company/haulmont/HaulmontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.haulmont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaulmontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
