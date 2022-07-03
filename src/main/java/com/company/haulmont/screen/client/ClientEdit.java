05273358210e986ee7e0b1a9f5f5396f4ccf4692
Vladislav <VladislavPozhar@yandex.ru>
2022-07-03 16:55:18 +0400

    added an image component to display the passport when added to the database.

--- src/main/java/com/company/haulmont/screen/client/ClientEdit.java
+++ src/main/java/com/company/haulmont/screen/client/ClientEdit.java
@@ -1,10 +1,28 @@
 package com.company.haulmont.screen.client;
 
+import io.jmix.ui.component.*;
 import io.jmix.ui.screen.*;
 import com.company.haulmont.entity.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.ByteArrayInputStream;
 
 @UiController("Client.edit")
 @UiDescriptor("client-edit.xml")
 @EditedEntityContainer("clientDc")
 public class ClientEdit extends StandardEditor<Client> {
+
+    @Autowired
+    private FileUploadField passportField;
+
+    @Autowired
+    private Image imageField;
+
+    @Subscribe("passportField")
+    public void onPassportFieldValueChange(HasValue.ValueChangeEvent<byte[]> event) {
+        imageField.setSource(StreamResource.class)
+                .setStreamSupplier(() -> new ByteArrayInputStream(event.getValue()));
+    }
+
+
 }
