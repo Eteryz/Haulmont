9c3f22cae59b6f5b2365b399fd337da49f41aaa7
Vladislav <VladislavPozhar@yandex.ru>
2022-07-05 22:15:45 +0400

    added sending a message to the mail when editing a contract

--- src/main/java/com/company/haulmont/app/EmailServiceBean.java
+++ src/main/java/com/company/haulmont/app/EmailServiceBean.java
@@ -7,7 +7,8 @@ import io.jmix.email.EmailInfoBuilder;
 import io.jmix.email.Emailer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.io.IOException;
+
+import java.util.Map;
 
 @Service
 public class EmailServiceBean {
@@ -21,6 +22,29 @@ public class EmailServiceBean {
                 .setSubject("Insurance contract")
                 .setFrom(null)
                 .setBody(editedEntity.toString())
+                .build();
+        emailer.sendEmail(emailInfo);
+    }
+
+    public void sendByEmail(Map<String, Object[]> map, String email) throws EmailException {
+        //TODO сделать красивый текст. Возможно добавить уникальный номер договора.
+        StringBuilder stringBuilder = new StringBuilder();
+        map.forEach((key, value) ->
+                stringBuilder
+                .append(key)
+                .append(":\t'")
+                .append(value[1].toString())
+                .append("'\t(")
+                .append("previous value '")
+                .append(value[0].toString())
+                .append("')")
+                .append("\n"));
+
+        EmailInfo emailInfo = EmailInfoBuilder.create()
+                .setAddresses(email)
+                .setSubject("Changes in the insurance contract!")
+                .setFrom(null)
+                .setBody(stringBuilder.toString())
                 .build();
         emailer.sendEmail(emailInfo);
     }
--- src/main/java/com/company/haulmont/screen/contract/ContractEdit.java
+++ src/main/java/com/company/haulmont/screen/contract/ContractEdit.java
@@ -3,11 +3,15 @@ package com.company.haulmont.screen.contract;
 import com.company.haulmont.app.EmailServiceBean;
 import io.jmix.email.EmailException;
 import io.jmix.ui.model.DataContext;
+import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.screen.*;
 import com.company.haulmont.entity.Contract;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @UiController("Contract.edit")
@@ -16,6 +20,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class ContractEdit extends StandardEditor<Contract> {
 
     private static final Logger log = LoggerFactory.getLogger(Contract.class);
+
+    /*
+    Contains field names and values that have been changed in the record
+    KEY - names changed attributes
+    VALUE - an array containing the {old, new} attribute values
+     */
+    private final Map<String, Object[]> mapChangesInTheRecord = new HashMap<>();
 
     //contract creation flag
     private boolean justCreated;
@@ -29,13 +40,16 @@ public class ContractEdit extends StandardEditor<Contract> {
     }
 
     @Subscribe(target = Target.DATA_CONTEXT)
-    public void onPostCommit(DataContext.PostCommitEvent event) {
+    public void onPostCommit(DataContext.PostCommitEvent event) throws EmailException {
         if (justCreated) {
-            try {
-                emailServiceBean.sendByEmail(getEditedEntity());
-            } catch (EmailException e) {
-                log.error("Error sending email", e);
-            }
+            emailServiceBean.sendByEmail(getEditedEntity());
+        }else {
+            emailServiceBean.sendByEmail(mapChangesInTheRecord,getEditedEntity().getClient().getEmail());
         }
+    }
+
+    @Subscribe(id = "contractDc", target = Target.DATA_CONTAINER)
+    public void onContractDcItemPropertyChange(InstanceContainer.ItemPropertyChangeEvent<Contract> event) {
+        mapChangesInTheRecord.put(event.getProperty(), new Object[]{event.getPrevValue(),event.getValue()});
     }
 }
