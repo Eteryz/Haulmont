db4c0183f654ec3bd9c1e7bceff7cb67ae43d440
Vladislav <VladislavPozhar@yandex.ru>
2022-07-08 16:51:58 +0400

    mail templates are now created in code

---
+++ src/main/java/com/company/haulmont/app/service/CreateTriggerServiceBean.java
@@ -0,0 +1,16 @@
+package com.company.haulmont.app.service;
+
+import io.jmix.core.DataManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateTriggerServiceBean {
+
+    @Autowired
+    private DataManager dataManager;
+
+    public void createTrigger() {
+
+    }
+}
