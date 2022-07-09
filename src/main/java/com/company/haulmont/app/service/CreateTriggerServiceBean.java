661dd47e1a1ccc54e4dc672e20e861aa1e4c5744
Vladislav <VladislavPozhar@yandex.ru>
2022-07-09 15:41:07 +0400

    implemented adding a task and a trigger for it

--- src/main/java/com/company/haulmont/app/service/CreateTriggerServiceBean.java
+++ src/main/java/com/company/haulmont/app/service/CreateTriggerServiceBean.java
@@ -1,6 +1,6 @@
 package com.company.haulmont.app.service;
 
-import io.jmix.core.DataManager;
+import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +8,18 @@ import org.springframework.stereotype.Service;
 public class CreateTriggerServiceBean {
 
     @Autowired
-    private DataManager dataManager;
+    private Scheduler scheduler;
 
-    public void createTrigger() {
+    public void addTaskWithTrigger(String nameJob, String groupJob, String nameTrigger, String groupTrigger, ScheduleBuilder responseTime) throws SchedulerException {
+        // define the job and tie it to our SendingEmailJob class
+        JobDetail job = JobBuilder.newJob(SendingEmailsJob.class)
+                .withIdentity(nameJob, groupJob)
+                .build();
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity(nameTrigger, groupTrigger)
+                .withSchedule(responseTime)
+                .build();
+        scheduler.scheduleJob(job,trigger);
 
     }
 }
